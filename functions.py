67ba9ea40b48cd71bac1b7d4a3017cbc8afdc459
Dee Dee LennonJones <elennonj@wellesley.edu>
2019-05-17 21:16:00 +0000

    inventory page works and is case checked

--- functions.py
+++ functions.py
@@ -80,6 +80,13 @@ def getIngredientKinds(conn):
     curs = conn.cursor(MySQLdb.cursors.DictCursor)
     curs.execute('select kind from ingredient where kind != "base" group by kind')
     return curs.fetchall()
+    
+def updateQuantity(conn,ingred_id,quantity):
+    curs = conn.cursor(MySQLdb.cursors.DictCursor)
+    curs.execute('update ingredient set quantity = (select * from (select quantity + %s from ingredient where iid=%s) as t1) where iid=%s',(quantity,ingred_id,ingred_id,))
+    curs.execute('select * from ingredient where iid = %s',(ingred_id,))
+    conn.commit()
+    return curs.fetchone()
 
 # Updates the payments table, and the user's current balance in the user table
 def makePayment(conn,username,method,amount,dt):
